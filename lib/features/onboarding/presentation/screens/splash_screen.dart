f5a99ab8792c612072554c56aebcd52b8a030eef
Cindy-dev <ikwuegbucynthia2000@gmail.com>
2025-04-01 19:47:39 +0100

    splash screen implementation

--- lib/features/onboarding/presentation/screens/splash_screen.dart
+++ lib/features/onboarding/presentation/screens/splash_screen.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:pixelfield/common/utils/app_extension.dart';
+import 'package:pixelfield/common/utils/pixelfield_routes.dart';
 import 'package:pixelfield/common/utils/pixelfield_strings.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -11,15 +13,33 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    navigateAfterSplash();
+  }
+
+  void navigateAfterSplash() {
+    Timer(
+      const Duration(seconds: 2),
+      () {
+        Navigator.pushReplacementNamed(
+            context, PixelFieldRoutes.myCollectionScreen);
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         height: context.deviceHeight(),
         decoration: BoxDecoration(
           image: DecorationImage(
+            fit: BoxFit.fill,
             image: AssetImage(PixelFieldStrings.background),
           ),
         ),
+        child: Image.asset(PixelFieldStrings.oneCask),
       ),
     );
   }
