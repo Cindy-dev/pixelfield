54227a02c44916c8586b2dad019445096ea1e6e7
Cindy-dev <ikwuegbucynthia2000@gmail.com>
2025-04-01 20:33:49 +0100

    welcome screen implementation

--- lib/features/collection/presentation/screens/welcome_screen.dart
+++ lib/features/collection/presentation/screens/welcome_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:pixelfield/common/utils/app_extension.dart';
 import 'package:pixelfield/common/utils/margin_util.dart';
 import 'package:pixelfield/common/utils/pixelfield_button.dart';
+import 'package:pixelfield/common/utils/pixelfield_routes.dart';
 import 'package:pixelfield/common/widgets/custom_scaffold.dart';
 
 class WelcomeScreen extends StatelessWidget {
@@ -20,7 +21,8 @@ class WelcomeScreen extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
           alignment: Alignment.bottomCenter,
           height: 264,
-          decoration: BoxDecoration(color: Color(0xff122329)),
+          decoration:
+              BoxDecoration(color: context.themeData.scaffoldBackgroundColor),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
@@ -37,10 +39,14 @@ class WelcomeScreen extends StatelessWidget {
                 children: [
                   Text("Have an account?", style: context.textTheme.bodyLarge),
                   const XMargin(10),
-                  Text(
-                    "Sign in first",
-                    style: context.textTheme.headlineLarge
-                        ?.copyWith(fontSize: 16, color: context.secondaryColor),
+                  InkWell(
+                    onTap: () => Navigator.of(context)
+                        .pushNamed(PixelFieldRoutes.myCollectionScreen),
+                    child: Text(
+                      "Sign in first",
+                      style: context.textTheme.headlineLarge?.copyWith(
+                          fontSize: 16, color: context.secondaryColor),
+                    ),
                   ),
                 ],
               )
