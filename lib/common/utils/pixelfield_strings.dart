bbb6f2742fcbb2ba8fc4bf562797b8f9d562cd5a
Cindy-dev <ikwuegbucynthia2000@gmail.com>
2025-04-01 19:34:51 +0100

    strings update

--- lib/common/utils/pixelfield_strings.dart
+++ lib/common/utils/pixelfield_strings.dart
@@ -1,8 +1,9 @@
 class PixelFieldStrings {
   final String background = "assets/images/background.png";
   final String oneCask = "assets/images/one_cask.png";
-  final String bottle = "assets/images/bottle.svg";
-  final String gearSix = "assets/images/GearSix.svg";
-  final String scan = "assets/images/scan.svg";
-  final String squaresFour = "assets/images/SquaresFour.svg";
+  final String bottle = "assets/images/svgs/bottle.svg";
+  final String gearSix = "assets/images/svgs/GearSix.svg";
+  final String scan = "assets/images/svgs/scan.svg";
+  final String squaresFour = "assets/images/svgs/SquaresFour.svg";
+  final String notification = "assets/images/svgs/notification.svg";
 }
